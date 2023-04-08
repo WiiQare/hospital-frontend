b77c7da9a340cc0cc6576cb2a8d8c931f6f496ef
Peter NDENGO <frdrcpeter@gmail.com>
2023-04-08 15:49:25 +0100

    Stat card

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,28 @@
 import Head from "next/head";
 
 import DashboardLayout from "../layouts/Dashboard";
-
-import VoucherHome from "../components/organisms/Dashboard/Voucher";
-import ProviderFilter from "../components/organisms/Dashboard/ProviderFilter";
 import Carousel from "../components/organisms/Dashboard/Carousel";
 import HistoryWallet from "../components/organisms/Wallet/History";
+import Stat from "../components/atoms/Card/Stat";
 
 const Page = () => {
 
-  return (
-    <>
-      <Head>
-        <title>JAMII - UNICEF CRYPTO</title>
-        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
-      </Head>
-      <Carousel />
+	return (
+		<>
+			<Head>
+				<title>JAMII - UNICEF CRYPTO</title>
+				<script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+			</Head>
+			<Carousel />
+			<Stat />
 			<HistoryWallet />
-      
-    </>
-  );
+
+		</>
+	);
 };
 
 Page.getLayout = (page) => (
-  <DashboardLayout className="space-y-8">{page}</DashboardLayout>
+	<DashboardLayout className="space-y-8">{page}</DashboardLayout>
 );
 
 export default Page;
