a4bcfa7605f7d46d912a9bee6e8788dcfa03c483
abhishandy <abhishek.sandy@gmail.com>
2023-05-19 17:40:37 -0400

    item history

---
+++ components/atoms/Tab/ItemHistory.test.js
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import ItemHistory from "./ItemHistory";
+
+describe("Item History", () => {
+  it("renders Item History component", () => {
+    render(
+      <ItemHistory
+        fullname={"John Doe"}
+        email={"john@example.com"}
+        createdAt={new Date("2023-02-01")}
+      />
+    );
+    expect(screen.getByText("View Details")).toBeInTheDocument();
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+    expect(screen.getByText("john@example.com")).toBeInTheDocument();
+  });
+});
