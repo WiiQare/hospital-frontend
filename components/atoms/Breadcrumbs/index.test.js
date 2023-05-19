fd0dc6324cf152352edd3f7f53c2d34290f28630
abhishandy <abhishek.sandy@gmail.com>
2023-05-19 14:36:15 -0400

    test: 2 atoms

---
+++ components/atoms/Breadcrumbs/index.test.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Breadcrumbs from ".";
+
+describe("Breadcrumbs", () => {
+  it("renders correctly", () => {
+    const items = [];
+    const component = render(<Breadcrumbs items={items} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("renders with items", () => {
+    const items = [
+      { item: "Home", link: "/" },
+      { item: "Products", link: "/products" },
+      { item: "1", link: "/products/1" },
+    ];
+    render(<Breadcrumbs items={items} />);
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Products")).toBeInTheDocument();
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+});
