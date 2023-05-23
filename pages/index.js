620bc486acfd0319d0fe1b9ddc289d6f5991db92
Peter NDENGO <frdrcpeter@gmail.com>
2023-05-23 16:20:52 +0100

    Dynamic homepage

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,21 @@ import Carousel from "../components/organisms/Dashboard/Carousel";
 import HistoryWallet from "../components/organisms/Wallet/History";
 import Stat from "../components/atoms/Card/Stat";
 import { BsFillCreditCardFill } from "react-icons/bs";
-import { FaEthereum } from "react-icons/fa";
+import { FaEthereum, FaUserAlt } from "react-icons/fa";
 import { useSession } from "next-auth/react";
+import Fetcher from "../lib/Fetcher";
+import Link from "next/link";
 
 const Page = () => {
-	const { status, data } = useSession();
+	const { data } = useSession();
+
+	const { data:result, isLoading, isError } = Fetcher(`/provider/transactions?providerId=${data.user.data.providerId}`, data.accessToken);
+	const { data:resultStat, isLoading:loadingStat, isError: errorStat } = Fetcher(`/provider/statistics?providerId=${data.user.data.providerId}`, data.accessToken);
 
 	return (
 		<>
 			<Head>
-				<title>JAMII - UNICEF CRYPTO</title>
+				<title>Dashboard - Hôpital</title>
 				<script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
 			</Head>
 			{/* <Carousel />
@@ -50,33 +55,41 @@ const Page = () => {
 						<div className="grid gap-6 w-full grid-cols-2 md:gap-4">
 							<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
 
-								<div className="">
-									<div className="bg-orange bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-orange">
-										<BsFillCreditCardFill />
-									</div>
-									<h3 className="font-semibold text-gray-500 mb-5">Carte Bancaire</h3>
+								{
+									loadingStat ? (<>Loading...</>) : (
+										<div className="">
+											<div className="bg-orange bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-orange">
+												<BsFillCreditCardFill />
+											</div>
+											<h3 className="font-semibold text-gray-500 mb-5">Montant total</h3>
 
-									<span className="font-semibold text-5xl text-gray-700 ">10</span>
-									<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
-										<BsFillCreditCardFill />
-										<span>Balance</span>
-									</div>
-								</div>
+											<span className="font-semibold text-xl text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "USD" }).format(resultStat.totalAmount)}</span>
+											<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
+												<BsFillCreditCardFill />
+												<span>Balance</span>
+											</div>
+										</div>
+									)
+								}
 							</div>
 							<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
 
+							{
+									loadingStat ? (<>Loading...</>) : (
 								<div className="">
 									<div className="bg-primary bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-primary">
-										<FaEthereum />
+										<FaUserAlt />
 									</div>
-									<h3 className="font-semibold text-gray-500 mb-5">Cryptomonnaie</h3>
+									<h3 className="font-semibold text-gray-500 mb-5">Total Patients</h3>
 
-									<span className="font-semibold text-5xl text-gray-700 ">32</span>
+									<span className="font-semibold text-xl text-gray-700 ">{resultStat.totalUniquePatients}</span>
 									<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
 										<BsFillCreditCardFill />
-										<span>Balance</span>
+										<span>Patient soigné</span>
 									</div>
 								</div>
+								)
+									}
 							</div>
 						</div>
 					</div>
@@ -84,106 +97,46 @@ const Page = () => {
 					<div className="max-w-screen-xl flex flex-col md:grid md:grid-cols-3 gap-6 mb-20 md:mb-10">
 
 						<div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
-							<div className="flex items-center justify-between mb-4">
-								<h5 className="text-xl font-semibold leading-none text-gray-600 flex flex-col">Statistique <span className="text-xs font-light text-gray-400">18 opérations effectués</span></h5>
-								<a href="#" className="text-sm font-medium text-blue-600 hover:underline ">
-									Voir tous
-								</a>
-							</div>
-							<div className="flow-root">
-								<ul role="list" className="divide-y divide-gray-200">
-									<li className="py-3 sm:py-4">
-										<div className="flex items-center space-x-4">
-											<div className="flex-shrink-0">
-												<img className="w-10 h-10 rounded-lg" src="https://source.unsplash.com/random" alt="Neil image" />
-											</div>
-											<div className="flex-1 min-w-0">
-												<p className="text-sm font-medium text-gray-900 truncate">
-													Neil Sims
-												</p>
-												<p className="text-xs text-gray-500 truncate ">
-													+243 814 978 651
-												</p>
-											</div>
-											<div className="inline-flex items-center text-base font-semibold text-gray-900">
-												$320
-											</div>
+							{
+								isLoading ? (<>Loading...</>) : (
+									<>
+										<div className="flex items-center justify-between mb-4">
+											<h5 className="text-xl font-semibold leading-none text-gray-600 flex flex-col">Statistique <span className="text-xs font-light text-gray-400">{result.length} opérations effectués</span></h5>
+											<Link href={"/transactions"} legacyBehavior>
+												<a className="text-sm font-medium text-blue-600 hover:underline">
+													Voir tous
+												</a>
+											</Link>
 										</div>
-									</li>
-									<li className="py-3 sm:py-4">
-										<div className="flex items-center space-x-4">
-											<div className="flex-shrink-0">
-												<img className="w-10 h-10 rounded-lg" src="https://source.unsplash.com/random" alt="Bonnie image" />
-											</div>
-											<div className="flex-1 min-w-0">
-												<p className="text-sm font-medium text-gray-900 truncate">
-													Bonnie Green
-												</p>
-												<p className="text-xs text-gray-500 truncate">
-													+243 814 978 651
-												</p>
-											</div>
-											<div className="inline-flex items-center text-base font-semibold text-gray-900">
-												$3467
-											</div>
+										<div className="flow-root">
+											<ul role="list" className="divide-y divide-gray-200">
+												{
+													result.map((transaction, index) => (
+														<li className="py-3 sm:py-4">
+															<div className="flex items-center space-x-4">
+																<div className="flex-shrink-0">
+																	<img className="w-10 h-10 rounded-lg" src={`https://ui-avatars.com/api/?uppercase=true&background=888&name=${transaction.voucher.patientId}&bold=true&color=FFF`} alt="Neil image" />
+																</div>
+																<div className="flex-1 min-w-0">
+																	<p className="text-sm font-medium text-gray-900 truncate">
+																	{transaction.voucher.patientId}
+																	</p>
+																	<p className="text-xs text-gray-500 truncate ">
+																		+243 814 978 651
+																	</p>
+																</div>
+																<div className="inline-flex items-center text-base font-semibold text-gray-900">
+																	{new Intl.NumberFormat("en-US", { style: 'currency', currency: transaction.voucher.currency }).format(transaction.voucher.amount)}
+																</div>
+															</div>
+														</li>
+													))
+												}
+											</ul>
 										</div>
-									</li>
-									<li className="py-3 sm:py-4">
-										<div className="flex items-center space-x-4">
-											<div className="flex-shrink-0">
-												<img className="w-10 h-10 rounded-lg" src="https://source.unsplash.com/random" alt="Michael image" />
-											</div>
-											<div className="flex-1 min-w-0">
-												<p className="text-sm font-medium text-gray-900 truncate">
-													Michael Gough
-												</p>
-												<p className="text-xs text-gray-500 truncate">
-													+243 814 978 651
-												</p>
-											</div>
-											<div className="inline-flex items-center text-base font-semibold text-gray-900">
-												$67
-											</div>
-										</div>
-									</li>
-									<li className="py-3 sm:py-4">
-										<div className="flex items-center space-x-4">
-											<div className="flex-shrink-0">
-												<img className="w-10 h-10 rounded-lg" src="https://source.unsplash.com/random" alt="Lana image" />
-											</div>
-											<div className="flex-1 min-w-0">
-												<p className="text-sm font-medium text-gray-900 truncate">
-													Lana Byrd
-												</p>
-												<p className="text-xs text-gray-500 truncate">
-													+243 814 978 651
-												</p>
-											</div>
-											<div className="inline-flex items-center text-base font-semibold text-gray-900">
-												$367
-											</div>
-										</div>
-									</li>
-									<li className="pt-3 pb-0 sm:pt-4">
-										<div className="flex items-center space-x-4">
-											<div className="flex-shrink-0">
-												<img className="w-10 h-10 rounded-lg" src="https://source.unsplash.com/random" alt="Thomas image" />
-											</div>
-											<div className="flex-1 min-w-0">
-												<p className="text-sm font-medium text-gray-900 truncate">
-													Thomes Lean
-												</p>
-												<p className="text-xs text-gray-500 truncate">
-													+243 814 978 651
-												</p>
-											</div>
-											<div className="inline-flex items-center text-base font-semibold text-gray-900">
-												$2367
-											</div>
-										</div>
-									</li>
-								</ul>
-							</div>
+									</>
+								)
+							}
 						</div>
 
 						<div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
@@ -299,41 +252,48 @@ const Page = () => {
 						<div className="flex flex-col w-full min-h-max gap-6">
 							<div className="md:grid flex gap-6 w-full md:grid-cols-2 md:gap-4">
 								<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
-
+								{
+									loadingStat ? (<>Loading...</>) : (
 									<div className="">
 										<div className="bg-orange bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-orange">
 											<BsFillCreditCardFill />
 										</div>
-										<h3 className="font-semibold text-gray-500 mb-5">Carte Bancaire</h3>
+										<h3 className="font-semibold text-gray-500 mb-5">Montant remboursé</h3>
 
-										<span className="font-semibold text-5xl text-gray-700 ">10</span>
+										<span className="font-semibold text-xl text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "USD" }).format(resultStat.totalRedeemedAmount)}</span>
 										<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
 											<BsFillCreditCardFill />
 											<span>Balance</span>
 										</div>
 									</div>
+									)}
 								</div>
 								<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
 
-									<div className="">
-										<div className="bg-primary bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-primary">
-											<FaEthereum />
-										</div>
-										<h3 className="font-semibold text-gray-500 mb-5">Cryptomonnaie</h3>
+									{
+										loadingStat ? (<>Loading...</>) : (
+										<div className="">
+											<div className="bg-primary bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-primary">
+												<FaEthereum />
+											</div>
+											<h3 className="font-semibold text-gray-500 mb-5">En attente de reboursement</h3>
 
-										<span className="font-semibold text-5xl text-gray-700 ">32</span>
-										<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
-											<BsFillCreditCardFill />
-											<span>Balance</span>
+											<span className="font-semibold text-xl text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "USD" }).format(resultStat.totalPendingAmount)}</span>
+											<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
+												<BsFillCreditCardFill />
+												<span>Balance</span>
+											</div>
 										</div>
-									</div>
+									)}
 								</div>
 							</div>
 
 
 							<div className="flex flex-col w-full h-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
-								<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-								<p className="font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+								<h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Vos demandes de retrait prennent un certains temps ?</h5>
+								<p className="font-normal text-gray-700 text-sm">
+									Le retrait de fonds chez WiiQare ne prend au maximum que 48h, le temps d'une courte vérification puis le fond vous sera déversé...
+								</p>
 							</div>
 
 						</div>
