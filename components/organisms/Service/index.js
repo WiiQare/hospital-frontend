ecb01e25933296fc963cb41e88ca81512578b251
Peter Ndengo <Frdrcpeter@gmail.com>
2023-05-25 16:39:23 +0100

    Services (#13)
    * Request security
    
    * Fix session
    
    * Add services
    
    * Put back session management
    
    * Redeemed design
    
    * Redeem features
    
    * Reload when finishe
    
    * Dynamic homepage
    
    * Change Stat screen
    
    * Change icons
    
    * Change on CDF
    
    * Remove english word
    
    * Word
    
    * Fix design
    
    * Fix session
    
    * Add services
    
    ---------
    
    Co-authored-by: bienvenu Zigabe <bienvenuzigabe544@gmail.com>

--- components/organisms/Service/index.js
+++ components/organisms/Service/index.js
@@ -3,10 +3,21 @@ import CardHeader from "../../atoms/Card/Header";
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from '@headlessui/react';
 import { TextField, TextareaAutosize } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useMutation } from "react-query";
+import LoadingButton from "../../atoms/Loader/LoadingButton";
+import { addService } from "../../../lib/helper";
+import { useSession } from "next-auth/react";
+import Toast from "../../atoms/Toast";
 
 const Service = () => {
 
+    const {data} = useSession();
     let [isOpen, setIsOpen] = useState(false)
+    const [state, setState] = useState({ type: 0, message: '' });
+
+    console.log(data);
 
     const closeModal = () => {
         setIsOpen(false)
@@ -15,6 +26,57 @@ const Service = () => {
     const openModal = () => {
         setIsOpen(true)
     }
+
+    const newServiceMutation = useMutation(addService, {
+        onSuccess: (res) => {
+
+            console.log(res);
+            if (res.code) {
+                setState({ type: 2, message: res.message ?? res.description })
+                setTimeout(() => {
+                    setState({ type: 0, message: "" })
+                }, 3000);
+
+            } else {
+                setState({ type: 1, message: "Service ajouté avec succès..." });
+                formik.handleReset()
+                setTimeout(() => {
+                    closeModal();
+                    setState({ type: 0, message: "" })
+                }, 3000);
+
+            };
+        }
+    });
+
+    const onSubmit = async (values) => {
+        if (Object.keys(values).length == 0) return console.log("Pas de données");
+
+        newServiceMutation.mutate({ ...values, price: 0, providerId: data.user.data.providerId, accessToken: data.accessToken })
+
+    };
+
+    const ValidationSchema = yup.object().shape({
+        name: yup.string().required("Nom du service est requis"),
+        description: yup.string(),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            description: ''
+        },
+        validationSchema: ValidationSchema,
+        onSubmit
+    })
+
+    const closeToast = () => {
+        setState({ type: 0, message: "" })
+    }
+
+    const renderError = (message) => (
+        <p className="text-xs text-red-600 font-light flex items-center gap-1 px-1">{message}</p>
+    );
 
     return (
         <div className="p-2 space-y-6 md:py-8 md:px-6 mb-20">
@@ -53,6 +115,8 @@ const Service = () => {
                     </Transition.Child>
                     <div className="flex items-center justify-center min-h-full p-4 text-center">
                         <div className="fixed inset-0 overflow-y-auto">
+                        {state.type > 0 ? state.type == 2 ? <Toast type={"danger"} message={state.message} close={closeToast} /> : (state.type == 1 ? <Toast type={"success"} message={state.message} close={closeToast} /> : <></>) : <></>}
+
                             <div className="flex items-center justify-center min-h-full p-4 text-center">
                                 <Transition.Child
                                     as={Fragment}
@@ -63,6 +127,7 @@ const Service = () => {
                                     leaveFrom="opacity-100 scale-100"
                                     leaveTo="opacity-0 scale-95"
                                 >
+                        
                                     <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl space-y-10">
                                         <Dialog.Title as="div" className="flex justify-between items-center mb-4">
                                             <h3 className="text-md font-semibold leading-6 text-gray-900">
@@ -71,36 +136,46 @@ const Service = () => {
 
                                         </Dialog.Title>
 
+                                        <form className="flex w-full  gap-3 flex-col" onSubmit={formik.handleSubmit}>
+                                            <div className="flex flex-col gap-1">
+                                                <TextField
+                                                    fullWidth
+                                                    type={"text"}
+                                                    className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+                                                    label="Nom du service"
+                                                    placeholder="Entrez nom du service"
+                                                    name="name"
+                                                    variant="outlined"
+                                                    {...formik.getFieldProps('name')}
+                                                />
 
-                                        <form className="flex w-full  gap-3 flex-col">
-                                            <TextField
-                                                fullWidth
-                                                type={"email"}
-                                                className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
-                                                label="Nom du service"
-                                                placeholder="Entrez nom du service"
-                                                name="name"
-                                                variant="outlined"
-                                            />
+                                                {formik.errors.name && formik.touched.name ? renderError(formik.errors.name) : <></>}
 
-                                            <TextField
-                                                fullWidth
-                                                type={"description"}
-                                                multiline
-                                                className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
-                                                label="Description"
-                                                placeholder="Description"
-                                                name="name"
-                                                variant="outlined"
-                                                rows={5}
-                                            />
+                                            </div>
+
+                                            <div className="flex flex-col gap-1">
+
+                                                <TextField
+                                                    fullWidth
+                                                    type={"description"}
+                                                    multiline
+                                                    className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+                                                    label="Description"
+                                                    placeholder="Description"
+                                                    name="description"
+                                                    variant="outlined"
+                                                    rows={5}
+                                                    {...formik.getFieldProps('description')}
+                                                />
+                                                {formik.errors.description && formik.touched.description ? renderError(formik.errors.description) : <></>}
+                                            </div>
+
 
                                             <div className="flex flex-row-reverse gap-4">
                                                 <button className=" bg-orange text-sm py-2 px-4 rounded-lg effect-up text-white" type="submit">
-                                                    {/* {newInviteMutation.isLoading ? <LoadingButton /> : 'Envoyer'} */}
-                                                    + Ajouter
+                                                    {newServiceMutation.isLoading ? <LoadingButton /> : '+ Ajouter'}
                                                 </button>
-                                               
+
                                             </div>
                                         </form>
 
