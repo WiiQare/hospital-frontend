d9e734ffd149bbffc8b40ff5248001a6b338fd1b
Abhi Shandilya <abhiShandy@users.noreply.github.com>
2023-06-05 21:18:25 -0400

    test: fix splidejs and TransactionTable (#19)

--- components/atoms/Table/Transaction.test.js
+++ components/atoms/Table/Transaction.test.js
@@ -1,10 +1,44 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import TransactionTable from "./Transaction";
+import { TableContext } from "../../organisms/Transaction";
+
+jest.mock("../../../lib/Fetcher", () => {
+  return {
+    __esModule: true,
+    default: jest.fn(() => {
+      return {
+        data: [
+          {
+            status: "pending",
+            voucher: {
+              patientId: "1",
+              currency: "EUR",
+              amount: 100,
+            },
+            createdAt: "2021-09-28T14:00:00.000Z",
+          },
+        ],
+        isLoading: false,
+        isError: false,
+      };
+    }),
+  };
+});
 
 describe("Transaction Table", () => {
   it("renders Transaction Table component", () => {
-    render(<TransactionTable />);
-    expect(screen.getByText("Name")).toBeInTheDocument();
+    render(
+      <TableContext.Provider
+        value={{
+          selected: [],
+          session: { user: { data: { providerId: "test" } } },
+          setIsChecked: () => {},
+        }}
+      >
+        <TransactionTable />
+      </TableContext.Provider>
+    );
+    expect(screen.getByText("Status")).toBeInTheDocument();
   });
 });
