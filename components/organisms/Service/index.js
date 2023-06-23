9a98f4d35f9ba102b370206939908043afee1925
Peter NDENGO <frdrcpeter@gmail.com>
2023-06-23 08:59:48 +0100

    Interface create Package

--- components/organisms/Service/index.js
+++ components/organisms/Service/index.js
@@ -50,12 +50,21 @@ const columns = [
 const Service = () => {
 
     const { data } = useSession();
-    let [isOpen, setIsOpen] = useState(false)
+    let [isOpen, setIsOpen] = useState(false);
+    let [isOpenForPackage, setIsOpenForPackage] = useState(false);
     const [state, setState] = useState({ type: 0, message: '' });
     const [selected, setSelected] = useState([]);
 
     const { data: result, isLoading, isError } = Fetcher(`/provider/${data.user.data.providerId}/service`, data.accessToken);
 
+
+    const closeModalForPackage = () => {
+        setIsOpenForPackage(false)
+    }
+
+    const openModalForPackage = () => {
+        setIsOpenForPackage(true)
+    }
 
     const closeModal = () => {
         setIsOpen(false)
@@ -103,8 +112,24 @@ const Service = () => {
 
     };
 
+    const submitPackage = async (values) => {
+        if (Object.keys(values).length == 0) return console.log("Pas de données");
+
+        if (parseInt(values.price) == NaN) {
+            setState({ type: 2, message: "Le prix doit être numérique" })
+            setTimeout(() => {
+                setState({ type: 0, message: "" })
+            }, 3000);
+
+            return null
+        }
+
+        console.log(values);
+
+    };
+
     const ValidationSchema = yup.object().shape({
-        name: yup.string().required("Nom du service est requis"),
+        name: yup.string().required("Nom est requis"),
         description: yup.string(),
         price: yup.number("Le prix doit être numérique").required("Le prix est requis"),
     });
@@ -119,6 +144,16 @@ const Service = () => {
         onSubmit
     })
 
+    const formikPackage = useFormik({
+        initialValues: {
+            name: '',
+            description: '',
+            price: ''
+        },
+        validationSchema: ValidationSchema,
+        onSubmit: submitPackage
+    })
+
     const closeToast = () => {
         setState({ type: 0, message: "" })
     }
@@ -129,7 +164,7 @@ const Service = () => {
 
     const handleChange = async ({ selectedRows }) => {
         setSelected([])
-        selectedRows.map(item => setSelected([...selected, item.transactionHash]))
+        setSelected(selectedRows)
     };
 
     return (
@@ -172,7 +207,7 @@ const Service = () => {
 
                                     {selected.length > 0 ? (
                                         <div className="absolute bottom-2 px-10">
-                                            <button className=" bg-orange text-xs md:text-sm py-2 px-4 rounded-lg effect-up text-white shadow" onClick={() => alert(`Création d'un package avec ${selected.length} services`)}>+ Créer un package</button>
+                                            <button className=" bg-orange text-xs md:text-sm py-2 px-4 rounded-lg effect-up text-white shadow" onClick={() => openModalForPackage()}>+ Créer un package</button>
                                         </div>
                                     ) : <></>}
 
@@ -256,14 +291,14 @@ const Service = () => {
 
                                             <div className="flex flex-col gap-1">
                                                 <FormControl fullWidth>
-                                                    <InputLabel htmlFor="outlined-adornment-amount">Prix (en $)</InputLabel>
+                                                    <InputLabel htmlFor="outlined-adornment-amount">Prix (en CDF)</InputLabel>
                                                     <OutlinedInput
                                                         id="outlined-adornment-amount"
                                                         type="number"
                                                         name="price"
                                                         onChange={(e) => formik.setFieldValue("price", e.target.value)}
                                                         startAdornment={<InputAdornment position="start">CDF</InputAdornment>}
-                                                        label="Prix (en $)"
+                                                        label="Prix (en CDF)"
                                                     />
                                                 </FormControl>
 
@@ -292,6 +327,150 @@ const Service = () => {
                                             <div className="flex flex-row-reverse gap-4">
                                                 <button className=" bg-orange text-sm py-2 px-4 rounded-lg effect-up text-white" type="submit">
                                                     {newServiceMutation.isLoading ? <LoadingButton /> : '+ Ajouter'}
+                                                </button>
+
+                                            </div>
+                                        </form>
+
+                                    </Dialog.Panel>
+                                </Transition.Child>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+
+            <Transition appear show={isOpenForPackage} as={Fragment}>
+                <Dialog as="div" className="relative z-50" onClose={closeModalForPackage}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+                    <div className="flex items-center justify-center min-h-full p-4 text-center">
+                        <div className="fixed inset-0 overflow-y-auto">
+                            {state.type > 0 ? state.type == 2 ? <Toast type={"danger"} message={state.message} close={closeToast} /> : (state.type == 1 ? <Toast type={"success"} message={state.message} close={closeToast} /> : <></>) : <></>}
+
+                            <div className="flex items-center justify-center min-h-full p-4 text-center">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95"
+                                >
+
+                                    <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl space-y-10">
+                                        <Dialog.Title as="div" className="flex justify-between items-center mb-4">
+                                            <h3 className="text-md font-semibold leading-6 text-gray-900">
+                                                Créer un package avec {selected.length} services
+                                            </h3>
+
+                                        </Dialog.Title>
+
+                                        <form className="flex w-full  gap-4 flex-col" onSubmit={formikPackage.handleSubmit}>
+                                            <div className="flex flex-col gap-1">
+                                                <TextField
+                                                    fullWidth
+                                                    type={"text"}
+                                                    className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+                                                    label="Nom du Package"
+                                                    placeholder="Entrez nom du Package"
+                                                    name="name"
+                                                    variant="outlined"
+                                                    {...formikPackage.getFieldProps('name')}
+                                                />
+
+                                                {formikPackage.errors.name && formikPackage.touched.name ? renderError(formikPackage.errors.name) : <></>}
+
+                                            </div>
+
+                                            <div className="flex flex-col gap-1">
+                                                <FormControl fullWidth>
+                                                    <InputLabel htmlFor="outlined-adornment-amount">Prix (en CDF)</InputLabel>
+                                                    <OutlinedInput
+                                                        id="outlined-adornment-amount"
+                                                        type="number"
+                                                        name="price"
+                                                        onChange={(e) => formikPackage.setFieldValue("price", e.target.value)}
+                                                        startAdornment={<InputAdornment position="start">CDF</InputAdornment>}
+                                                        label="Prix (en CDF)"
+                                                    />
+                                                </FormControl>
+
+                                                {formikPackage.errors.price && formikPackage.touched.price ? renderError(formikPackage.errors.price) : <></>}
+
+                                            </div>
+
+                                            <div className="flex flex-col gap-1">
+
+                                                <TextField
+                                                    fullWidth
+                                                    type={"description"}
+                                                    multiline
+                                                    className=" placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+                                                    label="Description"
+                                                    placeholder="Description"
+                                                    name="description"
+                                                    variant="outlined"
+                                                    rows={4}
+                                                    {...formikPackage.getFieldProps('description')}
+                                                />
+                                                {formikPackage.errors.description && formikPackage.touched.description ? renderError(formikPackage.errors.description) : <></>}
+                                            </div>
+
+
+                                            <div className="relative overflow-x-auto sm:rounded-lg">
+                                                <table className="w-full text-sm text-left text-gray-500">
+                                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                                                        <tr>
+                                                            <th scope="col" className="px-6 py-3">
+                                                                Nom du service
+                                                            </th>
+                                                            <th scope="col" className="px-6 py-3">
+                                                                Prix initial
+                                                            </th>
+                                                            <th scope="col" className="px-6 py-3">
+                                                                
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {
+                                                            selected.map(service => (
+                                                                <tr className="bg-white border-b hover:bg-gray-50">
+                                                                
+                                                                    <th scope="row" className="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap">
+                                                                       {service.name}
+                                                                    </th>
+                                                                    
+                                                                    <td className="px-6 py-4 text-xs">
+                                                                        {new Intl.NumberFormat("en-US", { style: 'currency', currency: "CDF" }).format(service.price)}
+                                                                    </td>
+                                                                    <td className="px-6 py-4">
+                                                                        <button className="font-medium text-blue-600 hover:underline text-xs">Rétirer</button>
+                                                                    </td>
+                                                                </tr>
+                                                            ))
+                                                        }
+                                                        
+                                                    </tbody>
+                                                </table>
+                                            </div>
+
+
+
+                                            <div className="flex flex-row-reverse gap-4">
+                                                <button className=" bg-orange text-sm py-2 px-4 rounded-lg effect-up text-white" type="submit">
+                                                    {newServiceMutation.isLoading ? <LoadingButton /> : 'Créer le package'}
                                                 </button>
 
                                             </div>
