b1997d04f34c545ce17fdd1402d4214a7bb032c1
Peter Ndengo <Frdrcpeter@gmail.com>
2023-07-10 16:08:50 +0300

    Add banner  (#40)
    * Request security
    
    * Fix session
    
    * Add services
    
    * Put back session management
    
    * Redeemed design
    
    * Redeem features
    
    * Reload when finishe
    
    * Dynamic homepage
    
    * Change Stat screen
    
    * Change icons
    
    * Change on CDF
    
    * Remove english word
    
    * Word
    
    * Fix design
    
    * Fix session
    
    * Add services
    
    * Fix page profil
    
    * Profile modification
    
    * Fix conflict
    
    * New Readme for hospital
    
    * Set new screen size
    
    * Scan page
    
    * Change screen to login
    
    * Button arround
    
    * Add space
    
    * Select All
    
    * Fix bug 1/2
    
    * Change table
    
    * Fix this padding
    
    * Nothing notification
    
    * Package ajouter
    
    * New Step
    
    * Autocomplete field
    
    * Scan details design
    
    * Fix responsivity
    
    * Set index don't great than 5
    
    * Button delete
    
    * Scan fix problem
    
    * Change price
    
    * Add Price when we add a new service
    
    * List service
    
    * List service when we scan voucher
    
    * Fixed bug
    
    * Interface create Package
    
    * Create new package
    
    * Calcul price when we select service
    
    * Fix this
    
    * Change language icon
    
    * Fix tab
    
    * Fix adding price
    
    * Formik errors
    
    * le bouton scanner et le barre
    
    * Fix this
    
    ---------
    
    Co-authored-by: bienvenu Zigabe <bienvenuzigabe544@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import Fetcher from "../lib/Fetcher";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useRouter } from 'next/router';
+import { MdQrCodeScanner } from "react-icons/md";
 
 const Page = () => {
 	const router = useRouter();
@@ -40,28 +41,48 @@ const Page = () => {
 
 				<section className=" flex flex-col gap-6 w-full">
 					<div className="w-full flex flex-col pt-6 md:grid md:grid-cols-3 gap-6">
-						<div className="flex justify-between items-end bg-gray-50 shadow-sm col-span-2 border border-gray-200 rounded-lg h-fit">
-							<div className=" p-8 md:p-12">
-								<a href="#" className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md mb-6">
-									<svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-										<path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-									</svg>
-									Vous revoilà !
-								</a>
-								<h1 className="text-purple  text-md md:text-lg font-normal mb-2">Bon retour, {data.user.data.names}</h1>
-								<p className="text-sm font-light text-gray-500 mb-6">
-									Votre dashboard pour prendre en main la compte WiiQare de l'hopital <span className="text-orange">{data.user.data.names}</span>
-								</p>
-								<a href={`tel:${data.user.data.phoneNumber}`} className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-orange effect-up">
-									Contact
-									<svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-								</a>
+						<div className="col-span-2 flex flex-col gap-6">
+
+							<div className="flex justify-between items-end bg-gray-50 shadow-sm  border border-gray-200 rounded-lg h-fit">
+								<div className="p-8 md:p-12">
+									<a href="#" className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md mb-6">
+										<svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+											<path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+										</svg>
+										Vous revoilà !
+									</a>
+									<h1 className="text-purple  text-md md:text-lg font-normal mb-2">Bon retour, {data.user.data.names}</h1>
+									<p className="text-sm font-light text-gray-500 mb-6">
+										Votre dashboard pour prendre en main la compte WiiQare de l'hopital <span className="text-orange">{data.user.data.names}</span>
+									</p>
+									<a href={`tel:${data.user.data.phoneNumber}`} className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-orange effect-up">
+										Contact
+										<svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+									</a>
+								</div>
+
+								<div className="p-8 h-full">
+									<img src="/images/sketch.png" alt="Sketch" className="h-full object-contain w-full" />
+								</div>
 							</div>
 
-							<div className="h-full">
-								<img src="/images/sketch.png" alt="Sketch" className="h-full object-cover w-full" />
+							<div className="flex justify-between items-end bg-gray-50 shadow-sm border border-gray-200 rounded-lg h-fit">
+								<div className="p-8 md:px-12 md:py-6 flex flex-col md:flex-row justify-between md:items-center gap-3 w-full h-full">
+
+									<p className="text-xl font-extrabold text-gray-500">
+										Scannez le Pass Santé du Patient
+									</p>
+									<Link href="/scan">
+										<button className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-orange effect-up gap-2">
+											<MdQrCodeScanner />
+											Scannez
+										</button>
+									</Link>
+								</div>
 							</div>
 						</div>
+
+
 						<div className="grid gap-6 w-full grid-cols-2 md:gap-4">
 							<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
 
@@ -103,7 +124,45 @@ const Page = () => {
 									)
 								}
 							</div>
+							<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
+								{
+									loadingStat ? (<>Loading...</>) : (
+										<div className="">
+											<div className="bg-orange bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-orange">
+												<BsFillCreditCardFill />
+											</div>
+											<h3 className="font-semibold text-gray-500 mb-5">Montant remboursé</h3>
+
+											<span className="font-semibold text-md text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "CDF" }).format(resultStat.totalRedeemedAmount)}</span>
+											<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
+												<BsFillCreditCardFill />
+												<span>Balance</span>
+											</div>
+										</div>
+									)}
+							</div>
+							<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
+
+								{
+									loadingStat ? (<>Loading...</>) : (
+										<div className="">
+											<div className="bg-primary bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-primary">
+												<RiSendToBack />
+											</div>
+											<h3 className="font-semibold text-gray-500 mb-5">En attente de reboursement</h3>
+
+											<span className="font-semibold text-md text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "CDF" }).format(resultStat.totalPendingAmount)}</span>
+											<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
+												<BsFillCreditCardFill />
+												<span>Balance</span>
+											</div>
+										</div>
+									)}
+							</div>
 						</div>
+
+
+
 					</div>
 
 					<div className="max-w-screen-xl flex flex-col md:grid md:grid-cols-3 gap-6 mb-20 md:mb-10">
@@ -269,43 +328,7 @@ const Page = () => {
 						</div> */}
 
 						<div className="flex flex-col w-full min-h-max gap-6">
-							<div className="md:grid flex gap-6 w-full md:grid-cols-2 md:gap-4">
-								<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
-									{
-										loadingStat ? (<>Loading...</>) : (
-											<div className="">
-												<div className="bg-orange bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-orange">
-													<BsFillCreditCardFill />
-												</div>
-												<h3 className="font-semibold text-gray-500 mb-5">Montant remboursé</h3>
 
-												<span className="font-semibold text-md text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "CDF" }).format(resultStat.totalRedeemedAmount)}</span>
-												<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
-													<BsFillCreditCardFill />
-													<span>Balance</span>
-												</div>
-											</div>
-										)}
-								</div>
-								<div className="bg-gray-50 w-full border border-gray-200 rounded-lg py-8 px-4 shadow-sm h-fit">
-
-									{
-										loadingStat ? (<>Loading...</>) : (
-											<div className="">
-												<div className="bg-primary bg-opacity-20 rounded-lg p-2 w-fit mb-2 text-primary">
-													<RiSendToBack />
-												</div>
-												<h3 className="font-semibold text-gray-500 mb-5">En attente de reboursement</h3>
-
-												<span className="font-semibold text-md text-gray-700 ">{new Intl.NumberFormat("en-US", { style: 'currency', currency: "CDF" }).format(resultStat.totalPendingAmount)}</span>
-												<div className="flex gap-2 text-green-400 items-center mt-3 text-sm">
-													<BsFillCreditCardFill />
-													<span>Balance</span>
-												</div>
-											</div>
-										)}
-								</div>
-							</div>
 
 
 							<div className="flex flex-col w-full h-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
