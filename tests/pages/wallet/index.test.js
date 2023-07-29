99f477a5ae2c4f0d1866961cbc922c6f84efa46d
abhishandy <abhishek.sandy@gmail.com>
2023-07-29 10:54:25 -0400

    test: voucher buy page

--- tests/pages/wallet/index.test.js
+++ tests/pages/wallet/index.test.js
@@ -1,9 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import Page from "../../../pages/wallet";
-// require("jest-fetch-mock").enableMocks();
-
-// fetch.mockResponse(JSON.stringify({ clientSecret: "test" }));
 
 describe("Wallet Page", () => {
   it("renders", () => {
