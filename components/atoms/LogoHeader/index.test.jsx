eb5140b7e748d2c932c4c0d8e1defdacfcb4e65d
abhishandy <abhishek.sandy@gmail.com>
2023-07-29 11:32:13 -0400

    test: logo header

---
+++ components/atoms/LogoHeader/index.test.jsx
@@ -0,0 +1,9 @@
+import Page from "./index";
+import { render } from "@testing-library/react";
+
+describe("LogoHeader", () => {
+  it("renders correctly", () => {
+    const { container } = render(<Page />);
+    expect(container).toMatchSnapshot();
+  });
+});
