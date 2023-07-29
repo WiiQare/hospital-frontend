06b337496a475b5501c9e52d94a94e0073d62e09
abhishandy <abhishek.sandy@gmail.com>
2023-07-29 10:51:20 -0400

    test: wallet page

--- tests/pages/wallet/index.test.js
+++ tests/pages/wallet/index.test.js
@@ -1,11 +1,11 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Page from "../../pages/checkout";
-require("jest-fetch-mock").enableMocks();
+import Page from "../../../pages/wallet";
+// require("jest-fetch-mock").enableMocks();
 
-fetch.mockResponse(JSON.stringify({ clientSecret: "test" }));
+// fetch.mockResponse(JSON.stringify({ clientSecret: "test" }));
 
-describe("Checkout Page", () => {
+describe("Wallet Page", () => {
   it("renders", () => {
     const { container } = render(<Page />);
     expect(container).toMatchSnapshot();
