a82eff9076fb3390fb09c54e553c1fbf0ca0c472
Bratu Sebastian <artweb11@gmail.com>
2023-08-26 18:55:27 +0300

    fix merge issue + update react testing lib

--- components/organisms/Transaction/index.js
+++ components/organisms/Transaction/index.js
@@ -77,81 +77,84 @@ const Transaction = () => {
         }
       });
 
-		swalWithBootstrapButtons.fire({
-			title: 'Etes-vous sûr ?',
-			text: "Votre demande de retrait de fond sera envoyé et traité sous peu par l'équipe WiiQare",
-			icon: 'question',
-			showCancelButton: true,
-			confirmButtonText: 'Oui, je suis sûr',
-			cancelButtonText: 'Non, annuler',
-			reverseButtons: true,
-			showLoaderOnConfirm: true,
-			preConfirm: (redeem) => {
+    swalWithBootstrapButtons
+      .fire({
+        title: 'Etes-vous sûr ?',
+        text: "Votre demande de retrait de fond sera envoyé et traité sous peu par l'équipe WiiQare",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Oui, je suis sûr',
+        cancelButtonText: 'Non, annuler',
+        reverseButtons: true,
+        showLoaderOnConfirm: true,
+        preConfirm: (redeem) => {
+          const Options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${session.user.data.access_token}`,
+            },
+            body: JSON.stringify({ transactionHashes: selected }),
+          };
 
-				const Options = {
-					method: "POST",
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': `Bearer ${session.user.data.access_token}`
-					},
-					body: JSON.stringify({transactionHashes: selected})
+          if (selected.length == 0)
+            return Swal.showValidationMessage(`Aucun élément sélectionner...`);
 
-				};
+          return fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/v1/provider/redeem-voucher`,
+            Options,
+          )
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(response.statusText);
+              }
+              return response.json();
+            })
+            .catch((error) => {
+              Swal.showValidationMessage(`Request failed: ${error}`);
+            });
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          location.reload();
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          selected.length > 0 &&
+            swalWithBootstrapButtons.fire(
+              'Annuler',
+              'Une prochaine fois peut-être',
+              'warning',
+            );
+        }
+      });
+  };
 
-				if (selected.length == 0) return Swal.showValidationMessage(`Aucun élément sélectionner...`)
-
-				return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/provider/redeem-voucher`, Options)
-				  .then(response => {
-					if (!response.ok) {
-					  throw new Error(response.statusText)
-					}
-					return response.json()
-				  })
-				  .catch(error => {
-					Swal.showValidationMessage(
-					  `Request failed: ${error}`
-					)
-				  })
-			  },
-			  allowOutsideClick: () => !Swal.isLoading()
-		}).then((result) => {
-			if (result.isConfirmed) {
-				location.reload()
-			} else if (result.dismiss === Swal.DismissReason.cancel) {
-
-				selected.length > 0 && swalWithBootstrapButtons.fire(
-					'Annuler',
-					'Une prochaine fois peut-être',
-					'warning'
-				)
-			}
-		})
-	};
-
-	return (
-		<div className="p-2 space-y-6 md:py-8 md:px-6 mb-20">
-			<CardHeader
-				title={"Historique de Transactions"}
-
-				filter={{
-					label: {
-						title: "Trier par",
-						className: "py-1 w-[auto]"
-					},
-					className: "w-[auto]",
-					icon: () => <CiCircleList />,
-					items: ["Tous", "Pass non-réclamé", "Montant remboursé"]
-				}}
-				added={selected.length > 0 ? true : false}
-				titleAdd={"Reclamer remboursement"}
-				addClick={() => handleClick()}
-				download={false}
-			/>
-			<TableContext.Provider value={{ selected, setSelected, isChecked, setIsChecked, session }}>
-				<TransactionTable />
-			</TableContext.Provider>
-		</div>
-	);
+  return (
+    <div className="p-2 space-y-6 md:py-8 md:px-6 mb-20">
+      <CardHeader
+        title={'Historique de Transactions'}
+        filter={{
+          label: {
+            title: 'Trier par',
+            className: 'py-1 w-[auto]',
+          },
+          className: 'w-[auto]',
+          icon: () => <CiCircleList />,
+          items: ['Tous', 'Pass non-réclamé', 'Montant remboursé'],
+        }}
+        added={selected.length > 0 ? true : false}
+        titleAdd={'Reclamer remboursement'}
+        addClick={() => handleClick()}
+        download={false}
+      />
+      <TableContext.Provider
+        value={{ selected, setSelected, isChecked, setIsChecked, session }}
+      >
+        <TransactionTable />
+      </TableContext.Provider>
+    </div>
+  );
 };
 
 export default Transaction;
