f9df21418704b5107ea0ec9a96220761a8ac7ff9
Peter Ndengo <Frdrcpeter@gmail.com>
2023-10-04 12:23:46 +0300

    Change API link (#51)
    * Request security
    
    * Fix session
    
    * Add services
    
    * Put back session management
    
    * Redeemed design
    
    * Redeem features
    
    * Reload when finishe
    
    * Dynamic homepage
    
    * Change Stat screen
    
    * Change icons
    
    * Change on CDF
    
    * Remove english word
    
    * Word
    
    * Fix design
    
    * Fix session
    
    * Add services
    
    * Fix page profil
    
    * Profile modification
    
    * Fix conflict
    
    * New Readme for hospital
    
    * Set new screen size
    
    * Scan page
    
    * Change screen to login
    
    * Button arround
    
    * Add space
    
    * Select All
    
    * Fix bug 1/2
    
    * Change table
    
    * Fix this padding
    
    * Nothing notification
    
    * Package ajouter
    
    * New Step
    
    * Autocomplete field
    
    * Scan details design
    
    * Fix responsivity
    
    * Set index don't great than 5
    
    * Button delete
    
    * Scan fix problem
    
    * Change price
    
    * Add Price when we add a new service
    
    * List service
    
    * List service when we scan voucher
    
    * Fixed bug
    
    * Interface create Package
    
    * Create new package
    
    * Calcul price when we select service
    
    * Fix this
    
    * Change language icon
    
    * Fix tab
    
    * Fix adding price
    
    * Formik errors
    
    * le bouton scanner et le barre
    
    * Fix this
    
    * Dynamic details
    
    * Add module to convert currency
    
    * Fix message
    
    * Finish details voucher on payment
    
    * Optional shortenHash
    
    * Change link API
    
    ---------
    
    Co-authored-by: bienvenu Zigabe <bienvenuzigabe544@gmail.com>

--- components/organisms/Transaction/index.js
+++ components/organisms/Transaction/index.js
@@ -1,108 +1,104 @@
-import { createContext, useState } from 'react';
-import { CiCircleList } from 'react-icons/ci';
-import CardHeader from '../../atoms/Card/Header';
-import TransactionTable from '../../atoms/Table/Transaction';
+import { createContext, useState } from "react";
+import { CiCircleList } from "react-icons/ci";
+import CardHeader from "../../atoms/Card/Header";
+import TransactionTable from "../../atoms/Table/Transaction";
 import Swal from 'sweetalert2';
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 export const TableContext = createContext();
 
 const Transaction = () => {
-  const [selected, setSelected] = useState([]);
-  const [isChecked, setIsChecked] = useState(false);
-  const { data: session } = useSession();
 
-  const handleClick = () => {
-    console.log(selected);
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton:
-          'bg-orange ml-6 py-3 px-4 rounded-lg uppercase font-semibold text-white',
-        cancelButton: 'btn btn-danger',
-        text: 'text-xs',
-      },
-      buttonsStyling: false,
-    });
+	const [selected, setSelected] = useState([]);
+	const [isChecked, setIsChecked] = useState(false);
+	const { data: session } = useSession();
 
-    swalWithBootstrapButtons
-      .fire({
-        title: 'Etes-vous sûr ?',
-        text: "Votre demande de retrait de fond sera envoyé et traité sous peu par l'équipe WiiQare",
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Oui, je suis sûr',
-        cancelButtonText: 'Non, annuler',
-        reverseButtons: true,
-        showLoaderOnConfirm: true,
-        preConfirm: (redeem) => {
-          const Options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${session.user.data.access_token}`,
-            },
-            body: JSON.stringify({ transactionHashes: selected }),
-          };
 
-          if (selected.length == 0)
-            return Swal.showValidationMessage(`Aucun élément sélectionner...`);
+	const handleClick = () => {
+		console.log(selected);
+		const swalWithBootstrapButtons = Swal.mixin({
+			customClass: {
+				confirmButton: 'bg-orange ml-6 py-3 px-4 rounded-lg uppercase font-semibold text-white',
+				cancelButton: 'btn btn-danger',
+				text: 'text-xs'
+			},
+			buttonsStyling: false
+		})
 
-          return fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/v1/provider/redeem-voucher`,
-            Options,
-          )
-            .then((response) => {
-              console.log( response );
-              
-              if (!response.ok) {
-                throw new Error(response.statusText);
-              }
-              return response.json();
-            })
-            .catch((error) => {
-              Swal.showValidationMessage(`Request failed: ${error}`);
-            });
-        },
-        allowOutsideClick: () => !Swal.isLoading(),
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          location.reload();
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-          selected.length > 0 &&
-            swalWithBootstrapButtons.fire(
-              'Annuler',
-              'Une prochaine fois peut-être',
-              'warning',
-            );
-        }
-      });
-  };
+		swalWithBootstrapButtons.fire({
+			title: 'Etes-vous sûr ?',
+			text: "Votre demande de retrait de fond sera envoyé et traité sous peu par l'équipe WiiQare",
+			icon: 'question',
+			showCancelButton: true,
+			confirmButtonText: 'Oui, je suis sûr',
+			cancelButtonText: 'Non, annuler',
+			reverseButtons: true,
+			showLoaderOnConfirm: true,
+			preConfirm: (redeem) => {
 
-  return (
-    <div className="p-2 space-y-6 md:py-8 md:px-6 mb-20">
-      <CardHeader
-        title={'Historique de Transactions'}
-        filter={{
-          label: {
-            title: 'Trier par',
-            className: 'py-1 w-[auto]',
-          },
-          className: 'w-[auto]',
-          icon: () => <CiCircleList />,
-          items: ['Tous', 'Pass non-réclamé', 'Montant remboursé'],
-        }}
-        added={selected.length > 0 ? true : false}
-        titleAdd={'Reclamer remboursement'}
-        addClick={() => handleClick()}
-        download={false}
-      />
-      <TableContext.Provider
-        value={{ selected, setSelected, isChecked, setIsChecked, session }}
-      >
-        <TransactionTable />
-      </TableContext.Provider>
-    </div>
-  );
+				const Options = {
+					method: "POST",
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${session.user.data.access_token}`
+					},
+					body: JSON.stringify({transactionHashes: selected})
+
+				};
+
+				if (selected.length == 0) return Swal.showValidationMessage(`Aucun élément sélectionner...`)
+
+				return fetch(`https://api.wiiqare.com/api/v1/provider/redeem-voucher`, Options)
+				  .then(response => {
+					if (!response.ok) {
+					  throw new Error(response.statusText)
+					}
+					return response.json()
+				  })
+				  .catch(error => {
+					Swal.showValidationMessage(
+					  `Request failed: ${error}`
+					)
+				  })
+			  },
+			  allowOutsideClick: () => !Swal.isLoading()
+		}).then((result) => {
+			if (result.isConfirmed) {
+				location.reload()
+			} else if (result.dismiss === Swal.DismissReason.cancel) {
+
+				selected.length > 0 && swalWithBootstrapButtons.fire(
+					'Annuler',
+					'Une prochaine fois peut-être',
+					'warning'
+				)
+			}
+		})
+	};
+
+	return (
+		<div className="p-2 space-y-6 md:py-8 md:px-6 mb-20">
+			<CardHeader
+				title={"Historique de Transactions"}
+
+				filter={{
+					label: {
+						title: "Trier par",
+						className: "py-1 w-[auto]"
+					},
+					className: "w-[auto]",
+					icon: () => <CiCircleList />,
+					items: ["Tous", "Pass non-réclamé", "Montant remboursé"]
+				}}
+				added={selected.length > 0 ? true : false}
+				titleAdd={"Reclamer remboursement"}
+				addClick={() => handleClick()}
+				download={false}
+			/>
+			<TableContext.Provider value={{ selected, setSelected, isChecked, setIsChecked, session }}>
+				<TransactionTable />
+			</TableContext.Provider>
+		</div>
+	);
 };
 
 export default Transaction;
